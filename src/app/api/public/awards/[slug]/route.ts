b12ea8ec1d001e44ac6cdb226e9ee27d187cf9af
Jadson Souza <jadsonsouza@Jadsons-MacBook-Pro.local>
2023-05-15 13:43:40 -0300

    feat: get params through context

--- src/app/api/public/awards/[slug]/route.ts
+++ src/app/api/public/awards/[slug]/route.ts
@@ -1,10 +1,11 @@
 import awards from "@/mocks/awards";
-import { NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, res: NextApiResponse) {
-  const splittedUrl = request.url.split("/");
-  const slug = splittedUrl[splittedUrl.length - 1];
+export async function GET(
+  request: Request,
+  { params }: { params: { slug: string } }
+) {
+  const { slug } = params;
   const award = awards.find((a) => a.slug === slug);
   return NextResponse.json(award);
 }
